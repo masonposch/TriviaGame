60c300c84100e82df03ad21eaa1e1fe8acb4d6c1
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-08-28 23:28:34 -0500

    added correct answers in JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
 
+var correctAnswer1 = "88 million";
+var correctAnswer2 = "A Clowder";
+var correctAnswer3 = "70%";
+var correctAnswer4 = "48.5 inches";
+var correctAnswer5 = "Bezoar";
 
 
 
