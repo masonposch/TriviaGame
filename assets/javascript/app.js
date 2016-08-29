24a0ecc8517b9dccc7aef48df0fd0181a73f9d93
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-08-29 02:15:16 -0500

    fixed Javascript variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,44 +7,31 @@ var unanswered = 0;
 var questions = [
 	firstQuestion={
 		question: "How many pet cats are there in the U.S.?",
-		answers: <input type="radio" name="answer" value="88 million" checked> 88 million<br>
-				 <input type="radio" name="answer" value="120 million"> 120 million<br>
-				 <input type="radio" name="answer" value="657,930"> 657+','+930<br>
-			     <input type="radio" name="answer" value="6 million"> 6 million<br>
+		answers: '<input type="radio" name="answer" value="88 million" checked> 88 million<br><input type="radio" name="answer" value="120 million"> 120 million<br><input type="radio" name="answer" value="657,930"> 657930<br><input type="radio" name="answer" value="6 million"> 6 million<br><input type="submit" value="Submit">'
 	},
 	
 	secondQuestion={
 		question: "What is a group of cats called?",
-		answers: <input type="radio" name="answer" value="A Herd" checked> A Herd<br>
-				 <input type="radio" name="answer" value="A Gaggle"> A Gaggle<br>
-				 <input type="radio" name="answer" value="A Clowder"> A Clowder<br>
-				 <input type="radio" name="answer" value="A Stampede"> A Stampede<br>
+		answers: '<input type="radio" name="answer" value="A Herd" checked> A Herd<br><input type="radio" name="answer" value="A Gaggle"> A Gaggle<br><input type="radio" name="answer" value="A Clowder"> A Clowder<br><input type="radio" name="answer" value="A Stampede"> A Stampede<br><input type="submit" value="Submit">'
 	},
 
 	thirdQuestion={
 		question: "How much of their lives do cats spend sleeping?",
-		answers: <input type="radio" name="answer" value="35%" checked> 35%<br>
-				 <input type="radio" name="answer" value="50%"> 50%<br>
-				 <input type="radio" name="answer" value="85%"> 85%<br>
-				 <input type="radio" name="answer" value="70%"> 70%<br>
+		answers: '<input type="radio" name="answer" value="35%" checked> 35%<br><input type="radio" name="answer" value="50%"> 50%<br><input type="radio" name="answer" value="85%"> 85%<br><input type="radio" name="answer" value="70%"> 70%<br><input type="submit" value="Submit">'
 	},
 
 	fourthQuestion={
 		question: "How long is the world's longest cat?",
-		answers: <input type="radio" name="answer" value="2 feet" checked> 2 feet<br>
-				 <input type="radio" name="answer" value="48.5 inches"> 48.5 inches<br>
-				 <input type="radio" name="answer" value="37 inches"> 37 inches<br>
-				 <input type="radio" name="answer" value="100 inches"> 100 inches<br>
+		answers: '<input type="radio" name="answer" value="2 feet" checked> 2 feet<br><input type="radio" name="answer" value="48.5 inches"> 48.5 inches<br><input type="radio" name="answer" value="37 inches"> 37 inches<br><input type="radio" name="answer" value="100 inches"> 100 inches<br><input type="submit" value="Submit">'
 	},
 
 	fifthQuestion={
 		question: "What is the scientific name for a \"hairball\"?",
-		answers: <input type="radio" name="answer" value="Pieza kake" checked> Pieza kake<br>
-				 <input type="radio" name="answer" value="Bezoar"> Bezoar<br>
-				 <input type="radio" name="answer" value="Nematoad"> Nematoad<br>
-				 <input type="radio" name="answer" value="Scaptia"> Scaptia<br> 
+		answers: '<input type="radio" name="answer" value="Pieza kake" checked> Pieza kake<br><input type="radio" name="answer" value="Bezoar"> Bezoar<br><input type="radio" name="answer" value="Nematoad"> Nematoad<br><input type="radio" name="answer" value="Scaptia"> Scaptia<br><input type="submit" value="Submit">' 
 	}
 ];
+
+var currentIndex = 0;
 
 var correctAnswer1 = "88 million";
 var correctAnswer2 = "A Clowder";
@@ -72,7 +59,7 @@ function stop(){
 
 
 window.onload = function(){
-	$('#startGame').on('click', firstQuestion);
+	$('#startGame').on('click', theGame);
 	$('#startGame').on('click', timer);
 }
 
@@ -80,8 +67,9 @@ window.onload = function(){
 function theGame(){
 	$('#startGame').addClass('hidden');
 	$('#timer').removeClass('hidden');
-	$('#question1').removeClass('hidden');
-
+	$('#question').removeClass('hidden');
+	$('#theQuestion').html(questions[currentIndex].question);
+	$('form').html(questions[currentIndex].answers);
 }
 
 
